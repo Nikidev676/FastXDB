de0685e596d2bba1f4e58b7b0871f67cf7c0382f
Nikitha <nikinikitha676@gmail.com>
2025-06-17 10:26:46 +0530

    Removed .gitignore, .gitattributes, and src folder

--- src/main/java/com/example/fastxprojdemo/jpa/UserAccountRegRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.fastxprojdemo.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.fastxprojdemo.entity.UserAccount;
-
-public interface UserAccountRegRepo extends JpaRepository<UserAccount, String> {
-}
